e3503ff658bf9f452217155645d2b132a81c39b0
Andrea Cifuentes <cifuentes17andrea@gmail.com>
2024-06-18 14:25:30 -0500

    Change

--- lib/DataBase/Dao/gastos_dao.dart
+++ lib/DataBase/Dao/gastos_dao.dart
@@ -44,4 +44,37 @@ class GastosDao {
   ''', [year.toString()]);
     return data;
   }
+
+  Future<List<GastosModel>> filtrarGastos({
+    DateTimeRange? dateRange,
+    List<String>? categories,
+    int? minAmount,
+    int? maxAmount,
+  }) async {
+    List<String> whereClauses = [];
+    List<dynamic> whereArgs = [];
+
+    if (dateRange != null) {
+      whereClauses.add('fecha >= ?');
+      whereArgs.add(DateFormat('yyyy-MM-dd').format(dateRange.start));
+      whereClauses.add('fecha <= ?');
+      whereArgs.add(DateFormat('yyyy-MM-dd').format(dateRange.end));
+    }
+    if (categories != null && categories.isNotEmpty) {
+      whereClauses.add('nombre IN (${categories.map((_) => '?').join(', ')})');
+      whereArgs.addAll(categories);
+    }
+    if (minAmount != null) {
+      whereClauses.add('valor >= ?');
+      whereArgs.add(minAmount);
+    }
+    if (maxAmount != null) {
+      whereClauses.add('valor <= ?');
+      whereArgs.add(maxAmount);
+    }
+    final whereString = whereClauses.isNotEmpty ? whereClauses.join(' AND ') : null;
+    final data = await database.query('Gastos', where: whereString, whereArgs: whereArgs);
+    return data.map((e) => GastosModel.fromJson(e)).toList();
+  }
 }
+
