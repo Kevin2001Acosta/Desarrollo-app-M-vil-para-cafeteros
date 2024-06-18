9613b825575714c00225e7049e0126bcbac210d3
Andrea Cifuentes <cifuentes17andrea@gmail.com>
2024-06-18 14:12:28 -0500

    finished expense view

--- lib/DataBase/Dao/gastos_dao.dart
+++ lib/DataBase/Dao/gastos_dao.dart
@@ -1,6 +1,7 @@
 import 'package:cafetero/DataBase/data_base_helper.dart';
 import 'package:cafetero/Models/gastos_model.dart';
 import 'package:intl/intl.dart';
+import 'package:flutter/material.dart'; 
 
 class GastosDao {
   final database = DatabaseHelper.instance.db;
@@ -32,4 +33,45 @@ class GastosDao {
         whereArgs: [DateFormat('yyyy-MM-dd').format(firstDayOfYear)]);
     return data.map((e) => GastosModel.fromJson(e)).toList();
   }
+
+  Future<List<GastosModel>> filtrarGastos({
+    DateTimeRange? dateRange,
+    List<String>? categories,
+    int? minAmount,
+    int? maxAmount,
+  }) async {
+    List<String> whereClauses = [];
+    List<dynamic> whereArgs = [];
+
+    if (dateRange != null) {
+      whereClauses.add('fecha >= ?');
+      whereArgs.add(DateFormat('yyyy-MM-dd').format(dateRange.start));
+      whereClauses.add('fecha <= ?');
+      whereArgs.add(DateFormat('yyyy-MM-dd').format(dateRange.end));
+    }
+    if (categories != null && categories.isNotEmpty) {
+      whereClauses.add('nombre IN (${categories.map((_) => '?').join(', ')})');
+      whereArgs.addAll(categories);
+    }
+    if (minAmount != null) {
+      whereClauses.add('valor >= ?');
+      whereArgs.add(minAmount);
+    }
+    if (maxAmount != null) {
+      whereClauses.add('valor <= ?');
+      whereArgs.add(maxAmount);
+    }
+    final whereString = whereClauses.isNotEmpty ? whereClauses.join(' AND ') : null;
+    final data = await database.query('Gastos', where: whereString, whereArgs: whereArgs);
+    return data.map((e) => GastosModel.fromJson(e)).toList();
+  }
 }
+
+
+  
+
+
+
+
+
+
--- lib/Screens/vista_gastos_page.dart
+++ lib/Screens/vista_gastos_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:data_table_2/data_table_2.dart'; // Importa el paquete de DataTable2
+import 'package:data_table_2/data_table_2.dart'; 
 import 'package:intl/intl.dart';
-import 'package:cafetero/DataBase/Dao/gastos_dao.dart'; // Asegúrate de importar los DAO y modelos necesarios
+import 'package:cafetero/DataBase/Dao/gastos_dao.dart'; 
 import 'package:cafetero/Models/gastos_model.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 class VerGastosPage extends StatefulWidget {
   final DateTimeRange? selectedDates;
@@ -23,47 +24,193 @@ class VerGastosPage extends StatefulWidget {
 }
 
 class _VerGastosPageState extends State<VerGastosPage> {
-  late List<GastosModel> gastosFiltrados; // Lista para almacenar los gastos filtrados
+  final scrollController = ScrollController();
+  late List<GastosModel> gastosFiltrados = [];
+  bool sortAscending = true;
+  int sortColumnIndex = 0;
+  List<Color> rowColors = [];
 
   @override
   void initState() {
     super.initState();
     cargarGastosFiltrados();
+    scrollController.addListener(onListen);
   }
 
-  // Función para cargar los gastos filtrados desde la base de datos
+  void onListen() {
+    setState(() {});
+  }
+
   void cargarGastosFiltrados() async {
-    // Lógica para filtrar los gastos según los parámetros recibidos
-     // Actualiza el estado para reflejar los cambios en la UI
+    final gastosDao = GastosDao();
+    final List<GastosModel> gastos = await gastosDao.filtrarGastos(
+      dateRange: widget.selectedDates,
+      categories: widget.selectedGastos,
+      minAmount: widget.valorMinGasto,
+      maxAmount: widget.valorMaxGasto,
+    );
+    setState(() {
+      gastosFiltrados = gastos;
+      rowColors = List.generate(gastos.length, (index) => _getRowColor(index));
+    });
+  }
+
+ Color _getRowColor(int index) {
+    return index.isOdd ? Colors.white : const Color.fromARGB(255, 205, 218, 166);
+  }
+
+  void onSortCategoria(int columnIndex, bool ascending) {
+    setState(() {
+      if (columnIndex == sortColumnIndex) {
+        sortAscending = !sortAscending;
+      } else {
+        sortColumnIndex = columnIndex;
+        sortAscending = ascending;
+      }
+      gastosFiltrados.sort((a, b) {
+        int cmp = a.nombre.toLowerCase().compareTo(b.nombre.toLowerCase());
+        return sortAscending ? cmp : -cmp;
+      });
+      rowColors = List.generate(gastosFiltrados.length, (index) => _getRowColor(index));
+    });
+  }
+
+  void onSortMonto(int columnIndex, bool ascending) {
+    setState(() {
+      if (columnIndex == sortColumnIndex) {
+        sortAscending = !sortAscending;
+      } else {
+        sortColumnIndex = columnIndex;
+        sortAscending = ascending;
+      }
+      gastosFiltrados.sort((a, b) {
+        int cmp = a.valor.compareTo(b.valor);
+        return sortAscending ? cmp : -cmp;
+      });
+      rowColors = List.generate(gastosFiltrados.length, (index) => _getRowColor(index));
+    });
+  }
+
+  void onSortFecha(int columnIndex, bool ascending) {
+    setState(() {
+      if (columnIndex == sortColumnIndex) {
+        sortAscending = !sortAscending;
+      } else {
+        sortColumnIndex = columnIndex;
+        sortAscending = ascending;
+      }
+      gastosFiltrados.sort((a, b) {
+        int cmp = a.fecha.compareTo(b.fecha);
+        return sortAscending ? cmp : -cmp;
+      });
+      rowColors = List.generate(gastosFiltrados.length, (index) => _getRowColor(index));
+    });
   }
 
   @override
-  Widget build(BuildContext context) {
+  void dispose() {
+    scrollController.removeListener(onListen);
+    super.dispose();
+  }
+
+  @override
+  @override
+Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Gastos Filtrados'),
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: DataTable2(
-            columns: [
-              // Define las columnas del DataTable2
-              DataColumn2(label: Text('Fecha')),
-              DataColumn2(label: Text('Tipo')),
-              DataColumn2(label: Text('Valor')),
-            ],
-            rows: gastosFiltrados.map((gasto) {
-              return DataRow(cells: [
-                DataCell(Text(DateFormat('yyyy-MM-dd').format(gasto.fecha))),
-                DataCell(Text(gasto.nombre)),
-                DataCell(Text(gasto.valor.toString())),
-              ]);
-            }).toList(),
-          ),
+        centerTitle: true,
+        title: const Text(
+          'GASTOS',
+          style: TextStyle(color: Colors.white),
         ),
+        iconTheme: const IconThemeData(color: Colors.white),
       ),
+      body: gastosFiltrados.isEmpty
+         ? const Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.info_outline,
+                      color: Colors.black,
+                      size: 25,
+                    ),
+                    SizedBox(width: 8),
+                    AutoSizeText(
+                      'No hay gastos para mostrar',
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                      maxLines: 2,
+                      minFontSize: 18.0,
+                      maxFontSize: 25.0,
+                    ),
+                  ],
+                ),
+              )
+          : DataTable2(
+              columnSpacing: 5,
+              headingRowHeight: 60, 
+              dataRowHeight: 50, 
+              minWidth: 450,
+              headingRowColor: MaterialStateColor.resolveWith(
+                  (states) => const Color.fromARGB(255, 255, 255, 255)), 
+              isHorizontalScrollBarVisible: true,
+              columns: [
+                DataColumn2(
+                  label: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Categoria ', style: TextStyle(color: Colors.black)),
+                      Icon(Icons.filter_list, color: Colors.black),
+                    ],
+                  ),
+                  onSort: onSortCategoria,
+                ),
+                DataColumn2(
+                  label: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Monto ', style: TextStyle(color: Colors.black)),
+                      Icon(Icons.filter_list, color: Colors.black),
+                    ],
+                  ),
+                  numeric: true,
+                  onSort: onSortMonto,
+                ),
+                DataColumn2(
+                  label: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Fecha', style: TextStyle(color: Colors.black)),
+                      Icon(Icons.filter_list, color: Colors.black),
+                    ],
+                  ),
+                  onSort: onSortFecha,
+                ),
+              ],
+              rows: gastosFiltrados.asMap().entries.map((entry) {
+                final index = entry.key;
+                final gasto = entry.value;
+                return DataRow(
+                  color: MaterialStateProperty.resolveWith<Color>((states) => rowColors[index]),
+                  cells: [
+                    DataCell(Align(alignment: Alignment.center, child: Text(gasto.nombre, style: const TextStyle(color: Colors.black)))), 
+                    DataCell(Align(alignment: Alignment.center, child: Text(gasto.valor.toString(), style: const TextStyle(color: Colors.black)))), 
+                    DataCell(Align(alignment: Alignment.center, child: Text(DateFormat('yyyy-MM-dd').format(gasto.fecha), style: const TextStyle(color: Colors.black)))),
+                  ],
+                );
+              }).toList(),
+              headingTextStyle: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+              dataTextStyle: const TextStyle(
+                fontSize: 16,
+              ),
+            ),
     );
   }
 }
+
 
