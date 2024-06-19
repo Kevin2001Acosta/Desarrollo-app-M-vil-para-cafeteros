ed1c2bd2ad68c103d39ae9aa28f8802682b88754
Jhosebro <jhosebro2108@gmail.com>
2024-06-19 02:27:34 -0500

    Fix Vista Dias Recogidas

--- lib/Screens/vista_dias_recogida_page.dart
+++ lib/Screens/vista_dias_recogida_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cafetero/DataBase/Dao/trabaja_dao.dart';
+import 'package:intl/intl.dart'; // Asegúrate de tener este paquete
 
 class VistaDiasRecogidaPage extends StatefulWidget {
   final int? idRecogida;
@@ -13,7 +14,7 @@ class VistaDiasRecogidaPage extends StatefulWidget {
 class _VistaDiasRecogidaPageState extends State<VistaDiasRecogidaPage> {
   bool _isLoading = true;
   bool _isDescending = true;
-  Map<String, dynamic> _datosRecogida = {};
+  List<Map<String, dynamic>> _datosRecogida = [];
 
   @override
   void initState() {
@@ -109,24 +110,26 @@ class _VistaDiasRecogidaPageState extends State<VistaDiasRecogidaPage> {
   }
 
   List<Widget> _buildJornalList() {
-    List<MapEntry<String, dynamic>> entries = _datosRecogida.entries.toList();
+    List<Map<String, dynamic>> sortedData = List.from(_datosRecogida);
 
-    entries.sort((a, b) {
+    sortedData.sort((a, b) {
       if (_isDescending) {
-        return b.key.compareTo(a.key);
+        return b['fecha'].compareTo(a['fecha']);
       } else {
-        return a.key.compareTo(b.key);
+        return a['fecha'].compareTo(b['fecha']);
       }
     });
 
-    return entries.map((entry) {
-      final nombre = entry.key;
-      final data = entry.value;
-      return _buildJornalCard(nombre, data);
+    return sortedData.map((entry) {
+      final nombre = entry['nombre'].toString();
+      final fecha = entry['fecha'].toString();
+      final kilosTrabajador = (entry['kilos_trabajador'] as num).toDouble();
+      final pago = (entry['pago'] as num).toDouble();
+      return _buildJornalCard(nombre, fecha, kilosTrabajador, pago);
     }).toList();
   }
 
-  Widget _buildJornalCard(String nombre, Map<String, dynamic> data) {
+  Widget _buildJornalCard(String nombre, String fecha, double kilosTrabajador, double pago) {
     return Card(
       color: const Color.fromARGB(255, 205, 218, 166),
       shape: RoundedRectangleBorder(
@@ -169,6 +172,19 @@ class _VistaDiasRecogidaPageState extends State<VistaDiasRecogidaPage> {
             Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                const Icon(Icons.date_range, color: Colors.black),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Text(
+                    fecha,
+                    style: const TextStyle(fontSize: 20, color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
                 const Icon(Icons.scale, color: Colors.black),
                 const SizedBox(width: 10),
                 Expanded(
@@ -181,7 +197,7 @@ class _VistaDiasRecogidaPageState extends State<VistaDiasRecogidaPage> {
                           style: TextStyle(color: Colors.black),
                         ),
                         TextSpan(
-                          text: data['kilos_trabajador'].toString(),
+                          text: kilosTrabajador.toString(),
                           style: const TextStyle(color: Colors.black),
                         ),
                       ],
@@ -204,7 +220,7 @@ class _VistaDiasRecogidaPageState extends State<VistaDiasRecogidaPage> {
                           style: TextStyle(color: Colors.black),
                         ),
                         TextSpan(
-                          text: data['pago'].toString(),
+                          text: pago.toString(),
                           style: const TextStyle(color: Colors.black),
                         ),
                       ],
